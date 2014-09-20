260841848aa56cba3e192353164b1b5a9cc8e571
Mike Corrigan <michael.corrigan@motorola.com>
2014-09-19 22:08:19 -0500

    xnodes
    Signed-off-by: Mike Corrigan <michael.corrigan@motorola.com>

--- x.py
+++ x.py
@@ -202,59 +202,9 @@ class TestPublishment(unittest.TestCase):
         p = publishment(content='content', ttl=12)
         self.assertIsNotNone(repr(p))
 
-class node(object):
-    def __init__(self, topic):
-        self.topic = topic
-        self.publishments = []
-        self.subscriptions = []
-
-    def publish(self, publishment, endpoint):
-        if not self.topic.can_publish(endpoint.perm):
-            raise PermissionError('publish failed, topic perm=' + str(self.topic.from_perm) + ', endpoint perm=' + str(endpoint.perm))
-
-        self.publishments.append(publishment)
-
-        for subscription in self.subscriptions:
-            self.notify(subscription, publishment)
-
-    def subscribe(self, endpoint):
-        if not self.topic.can_subscribe(endpoint.perm):
-            raise PermissionError('subscribe failed, topic perm=' + str(self.topic.to_perm) + ', endpoint perm=' + str(endpoint.perm))
-
-        self.subscriptions.append(endpoint)
-        self.notify(endpoint, self.latest())
-
-    def read(self, endpoint):
-        if not self.topic.can_subscribe(endpoint.perm):
-            raise PermissionError('read failed, topic perm=' + str(self.topic.to_perm) + ', endpoint perm=' + str(endpoint.perm))
-
-        return self.latest()
-
-    def latest(self):
-        if not self.publishments:
-            return None
-
-        return self.publishments[-1]
-
-    def notify(self, subscription, publishment):
-        if not self.topic.to_perm.has_permission(subscription.perm):
-            raise PermissionError('notify failed, topic perm=' + str(self.topic.to_perm) + ', endpoint perm=' + str(subscription.perm))
-
-        subscription(publishment)
-
-    def __str__(self):
-        return 'topic=' + str(self.topic)
-
-    def __repr__(self):
-        return 'topic=' + str(self.topic) + ', subscriptions=' + ', publishments='
-
-class TestNode(unittest.TestCase):
-    def test_init_error(self):
-        self.assertRaises(TypeError, node)
-
 class xnode(object):
-    def __init__(self, name, parent=None):
-        self.name = name
+    def __init__(self, key, parent=None):
+        self.key = key
         self.parent = parent
         if self.parent:
             self.parent.add_child(self)
@@ -265,10 +215,25 @@ class xnode(object):
             self.parent.remove_child(self)
 
     def add_child(self, child):
-        self.children[child.name] = child
+        self.children[child.key] = child
 
     def remove_child(self, child):
-        del self.children[child.name]
+        del self.children[child.key]
+
+    def find_closest(self, keys):
+        if not keys:
+            return self, ()
+
+        try:
+            n = self
+            while len(keys):
+                subkey = keys[0]
+                n = n.children[subkey]
+                keys.pop(0)
+        except KeyError:
+            pass
+
+        return n, keys
 
     def up(self, list=None):
         if list is None:
@@ -287,7 +252,7 @@ class xnode(object):
         if prefix:
             list.append(self)
 
-        for name, child in self.children.items():
+        for key, child in self.children.items():
             child.down(list)
 
         if not prefix:
@@ -295,32 +260,11 @@ class xnode(object):
 
         return list
 
-    def get_descendent(self, names, separator='.'):
-        if not names:
-            return self
-
-        if isinstance(names, str):
-            # If the name is specified as a string, break into a sequence.
-            names = names.split(separator)
-        elif hasattr(names, '__iter__') and not hasattr(names, 'pop'):
-            # If name is iter-able, but not pop-able, then convert it into a list.
-            names = list(names)
-
-        return self.__get_descendent__(names)
-
-    def __get_descendent__(self, names):
-        if not names:
-            return self
-
-        name = names.pop(0)
-
-        return self.children[name].__get_descendent__(names)
-
     def __str__(self):
-        return 'name=' + str(self.name)
+        return 'key=' + str(self.key)
 
     def __repr__(self):
-        return 'name=' + repr(self.name) + ', parent=' + repr(self.parent) + ', children=' + repr(self.children)
+        return 'key=' + repr(self.key) + ', parent=' + repr(self.parent) + ', children=' + repr(self.children)
 
 class TestXNode(unittest.TestCase):
     def setUp(self):
@@ -339,9 +283,9 @@ class TestXNode(unittest.TestCase):
     def test_init_none(self):
         self.assertRaises(TypeError, xnode)
 
-    def test_init_name(self):
-        n = xnode('name')
-        self.assertEqual('name', n.name)
+    def test_init_key(self):
+        n = xnode('key')
+        self.assertEqual('key', n.key)
         self.assertIsNone(n.parent)
         self.assertEqual(0, len(n.children))
 
@@ -374,24 +318,30 @@ class TestXNode(unittest.TestCase):
         self.assertEqual(self.r, l[-1])
 
     def test_get_by_none(self):
-        n = self.r.get_descendent(None)
+        n, subkeys = self.r.find_closest(None)
         self.assertEqual(self.r, n)
+        self.assertEqual(0, len(subkeys))
 
     def test_get_by_empty(self):
-        n = self.r.get_descendent('')
+        n, subkeys = self.r.find_closest('')
         self.assertEqual(self.r, n)
+        self.assertEqual(0, len(subkeys))
 
-    def test_get_by_string(self):
-        n = self.r.get_descendent('a.ab.aba.abaa')
-        self.assertEqual(self.abaa, n)
+#    def test_get_by_string(self):
+#        n = self.r.find_closest('a.ab.aba.abaa')
+#        self.assertEqual(self.abaa, n)
 
-    def test_get_by_sequence(self):
-        n = self.r.get_descendent(('a', 'ab', 'aba', 'abaa'))
-        self.assertEqual(self.abaa, n)
+#    def test_get_by_sequence(self):
+#        n = self.r.find_closest(('a', 'ab', 'aba', 'abaa'))
+#        self.assertEqual(self.abaa, n)
 
     def test_get_by_list(self):
-        n = self.r.get_descendent(['a', 'ab', 'aba', 'abaa'])
+        n, subkeys = self.r.find_closest(['a', 'ab', 'aba', 'abaa'])
         self.assertEqual(self.abaa, n)
+        self.assertEqual(0, len(subkeys))
+
+#    def test_get_by_string_missing(self):
+#        self.assertRaises(KeyError, self.r.find_closest, 'a.ab.aba.abaa.abaaa')
 
     def test_str(self):
         self.assertIsNotNone(str(self.r))
@@ -399,39 +349,191 @@ class TestXNode(unittest.TestCase):
     def test_repr(self):
         self.assertIsNotNone(repr(self.r))
 
-class store(object):
-    def __init__(self):
-        self.endpoints = set()
-        self.nodes = {}
+class xnodes(object):
+    def __init__(self, separator='.'):
+        self.root = node('<root>')
+        self.separator = separator
 
-    def create_endpoint(self, endpoint):
-        self.endpoints.add(endpoint)
+    def check_keys(self, keys):
+        if isinstance(keys, str):
+            return keys.split(self.separator)
+        elif not hasattr(keys, '__iter__'):
+            raise TypeError('invalid keys')
+        elif not hasattr(keys, 'pop'):
+            # If keys is iter-able, but not pop-able, then convert it into a list.
+            return list(keys)
 
-    def delete_endpoint(self, endpoint):
-        # TODO: remove subscriptions
-        self.endpoints.remove(endpoint)
+        return keys
 
-    def create_node(self, topic):
-        self.nodes[topic] = node(topic)
+    def find_closest(self, keys):
+        keys = self.check_keys(keys)
+        return self.root.find_closest(keys)
 
-    def delete_node(self, topic):
-        # TODO: remove subscriptions
-#        del topic self.nodes
-        pass
+    def has_node(self, keys):
+        parent, subkeys = self.find_closest(keys)
+        return not len(subkeys)
 
-    def get_node(self, topic):
-        return self.nodes[topic]
+    def get_node(self, keys):
+        parent, subkeys = self.find_closest(keys)
+        if not len(subkeys):
+            return parent
 
-    def get_or_create_node(self, topic):
-        if not topic in self.nodes:
-            self.create_node(topic)
+        return None
 
-        return self.get_node(topic)
+    def create_node(self, keys):
+        if not keys:
+            return self.root
+
+        keys = self.check_keys(keys)
+
+        # Find the closest existing node.
+        n, subkeys = self.find_closest(keys)
+
+        # Create new nodes.
+        for subkey in keys:
+            n = xnode(subkey, n)
+
+        return n
+
+    def delete_node(self, keys):
+        n = self.get_node(keys)
+        if not n:
+            return None
+
+        n.delete()
+
+        return n
+
+class TestXNodes(unittest.TestCase):
+    def setUp(self):
+        self.x = xnodes()
+
+    def test_check_keys_tuple(self):
+        keys = self.x.check_keys(('a', 'aa', 'aaa'))
+        self.assertTrue(isinstance(keys, list))
+
+    def test_check_keys_list(self):
+        keys = self.x.check_keys(['a', 'aa', 'aaa'])
+        self.assertTrue(isinstance(keys, list))
+
+    def test_check_keys_none(self):
+        self.assertRaises(TypeError, self.x.check_keys, None)
+
+    def test_check_keys_int(self):
+        self.assertRaises(TypeError, self.x.check_keys, 1)
+
+    def test_check_keys_string(self):
+        keys = self.x.check_keys('a.aa.aaa')
+        self.assertTrue(isinstance(keys, list))
+
+    def test_find_closest_empty(self):
+        parent, subkeys = self.x.find_closest(['a', 'aa', 'aaa'])
+        self.assertEqual(self.x.root, parent)
+        self.assertEqual(3, len(subkeys))
+
+    def test_create(self):
+        a = self.x.create_node(['a'])
+        self.assertEqual('a', a.key)
+        self.assertEqual(self.x.root, a.parent)
+
+        aa = self.x.create_node(['a', 'aa'])
+        self.assertEqual('aa', aa.key)
+        self.assertEqual(a, aa.parent)
+
+    def test_has_node(self):
+        # setup
+        a = self.x.create_node(['a'])
+        aa = self.x.create_node(['a', 'aa'])
+
+        self.assertTrue(self.x.has_node(['a']))
+        self.assertTrue(self.x.has_node(['a', 'aa']))
+        self.assertFalse(self.x.has_node(['a', 'aa', 'aaa']))
+
+    def test_has_node(self):
+        # setup
+        a = self.x.create_node(['a'])
+        aa = self.x.create_node(['a', 'aa'])
+
+        self.assertEqual(a, self.x.get_node(['a']))
+        self.assertEqual(aa, self.x.get_node(['a', 'aa']))
+        self.assertIsNone(self.x.get_node(['a', 'aa', 'aaa']))
+
+    def test_delete(self):
+        # setup
+        a = self.x.create_node(['a'])
+        aa = self.x.create_node(['a', 'aa'])
+
+        self.assertIsNone(self.x.delete_node(['a', 'aa', 'aaa']))
+        self.assertEqual(aa, self.x.delete_node(['a', 'aa']))
+        self.assertIsNone(self.x.delete_node(['a', 'aa']))
+        self.assertEqual(a, self.x.delete_node(['a']))
+        self.assertIsNone(self.x.delete_node(['a']))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class node(xnode):
+    def __init__(self, topic, parent=None):
+        xnode.__init__(self, key=topic, parent=parent)
+        self.publishments = []
+        self.subscriptions = []
+
+    def publish(self, publishment, endpoint):
+        if not self.key.can_publish(endpoint.perm):
+            raise PermissionError('publish failed, topic perm=' + str(self.key.from_perm) + ', endpoint perm=' + str(endpoint.perm))
+
+        self.publishments.append(publishment)
+
+        for subscription in self.subscriptions:
+            self.notify(subscription, publishment)
+
+    def subscribe(self, endpoint):
+        if not self.key.can_subscribe(endpoint.perm):
+            raise PermissionError('subscribe failed, topic perm=' + str(self.key.to_perm) + ', endpoint perm=' + str(endpoint.perm))
+
+        self.subscriptions.append(endpoint)
+        self.notify(endpoint, self.latest())
+
+    def read(self, endpoint):
+        if not self.key.can_subscribe(endpoint.perm):
+            raise PermissionError('read failed, topic perm=' + str(self.key.to_perm) + ', endpoint perm=' + str(endpoint.perm))
+
+        return self.latest()
+
+    def latest(self):
+        if not self.publishments:
+            return None
+
+        return self.publishments[-1]
+
+    def notify(self, subscription, publishment):
+        if not self.key.to_perm.has_permission(subscription.perm):
+            raise PermissionError('notify failed, topic perm=' + str(self.key.to_perm) + ', endpoint perm=' + str(subscription.perm))
+
+        subscription(publishment)
+
+    def __str__(self):
+        return 'topic=' + str(self.key)
+
+    def __repr__(self):
+        return 'topic=' + str(self.key) + ', subscriptions=' + ', publishments='
 
 class service(object):
     def __init__(self):
         self.endpoints = set()
-        self.nodes = {}
+        self.root = node('<root>')
+        self.separator = '.'
 
     def create_endpoint(self, endpoint):
         self.endpoints.add(endpoint)
@@ -441,21 +543,60 @@ class service(object):
         self.endpoints.remove(endpoint)
 
     def create_node(self, topic):
-        self.nodes[topic] = node(topic)
+        if not topic:
+            return self.root
+
+        if isinstance(topic, str):
+            # If the key is specified as a string, break into a sequence.
+            topic = topic.split(self.separator)
+        elif hasattr(topic, '__iter__') and not hasattr(topic, 'pop'):
+            # If key is iter-able, but not pop-able, then convert it into a list.
+            topic = list(topic)
+
+        return self.__create_node__(topic)
+
+    def __create_node__(self, topic):
+        n = self.root
+
+        # Find the lowest node that is already created.
+        try:
+            while True:
+                subtopic = topic[0]
+                n = n.children[subtopic]
+                topic.pop(0)
+        except KeyError:
+            pass
+
+        # Create any new nodes.
+        for subtopic in topic:
+            n = node(subtopic, n)
 
     def delete_node(self, topic):
         # TODO: remove subscriptions
-#        del topic self.nodes
+        # TODO: remove node
         pass
 
+    def has_node(self, topic):
+        try:
+            n = self.root
+            while True:
+                subtopic = topic[0]
+                n = n.children[subtopic]
+                topic.pop(0)
+        except KeyError:
+            return False
+
+        return True
+
     def get_node(self, topic):
-        return self.nodes[topic]
+        return self.root.get_descendent(topic)
 
     def get_or_create_node(self, topic):
-        if not topic in self.nodes:
+        try:
+            return self.get_node(topic)
+        except KeyError:
             self.create_node(topic)
-
-        return self.get_node(topic)
+            return self.get_node(topic)
 
     def publish(self, topic, publishment, endpoint):
         n = self.get_or_create_node(topic)
@@ -469,10 +610,39 @@ class service(object):
         n = self.get_node(topic)
         return n.read(endpoint)
 
+
+    def get_descendent(self, names, separator='.'):
+        if not names:
+            return self
+
+        if isinstance(names, str):
+            # If the key is specified as a string, break into a sequence.
+            names = names.split(separator)
+        elif hasattr(names, '__iter__') and not hasattr(names, 'pop'):
+            # If key is iter-able, but not pop-able, then convert it into a list.
+            names = list(names)
+
+        return self.__get_descendent__(names)
+
+
+
+
+class TestService(unittest.TestCase):
+    def test_create(self):
+        s = service()
+        s.__create_node__(['a', 'aa', 'aaa'])
+        s.__create_node__(['a', 'aa', 'aab'])
+        s.__create_node__(['b', 'ba', 'baa'])
+        s.create_node('b.ba.baa.baaa')
+        l = s.root.down()
+        for n in l:
+            print(n.key)
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
-
+    """
     ea = endpoint(perm(gid='user', uid='mike'))
     eb = endpoint(perm(gid='admin', uid='chloe'))
     print('ea', str(ea))
@@ -528,7 +698,7 @@ if __name__ == '__main__':
 
     n.delete_endpoint(ea)
     n.delete_endpoint(eb)
-
+    """
     logger.setLevel(level=logging.WARNING)
     unittest.main()
 
